3491e9ebcb64a9600c2abdcc3db0aba47a72c961
William <ywwwtseng@gmail.com>
2025-11-27 14:39:54 +0800

    ton generate and recover wallet

--- src/KeyVaultService/KeyVaultService.spec.ts
+++ src/KeyVaultService/KeyVaultService.spec.ts
@@ -3,7 +3,7 @@ import crypto from 'crypto';
 import { KeyVaultService } from '..';
 
 describe('KeyVaultService', () => {
-  test('solana generate and recover', () => {
+  test('solana generate and recover wallet', () => {
     const keyVaultService = new KeyVaultService(crypto.randomBytes(32));
     const wallet = keyVaultService.solana.generate();
     const recovered = keyVaultService.solana.recover(
@@ -17,13 +17,27 @@ describe('KeyVaultService', () => {
     );
   });
 
-  test('evm generate and recover', () => {
+  test('evm generate and recover wallet', () => {
     const keyVaultService = new KeyVaultService(crypto.randomBytes(32));
     const wallet = keyVaultService.evm.generate();
     const recovered = keyVaultService.evm.recover(wallet.privateKeyEncrypted);
 
     expect(wallet.address).toBe(recovered.address);
 
+    expect(keyVaultService.decrypt(wallet.privateKeyEncrypted)).toBe(
+      recovered.privateKey
+    );
+  });
+
+  test('ton generate and recover wallet', async () => {
+    const keyVaultService = new KeyVaultService(crypto.randomBytes(32));
+    const wallet = await keyVaultService.ton.generate();
+    const recovered = keyVaultService.ton.recover(wallet.privateKeyEncrypted);
+
+    // 地址存在但合約尚未部署，使用 non-bounceable UQ...，才能保證能接收 TON 不會 bounced 回去
+    expect(wallet.address.toString({ urlSafe: true, bounceable: false })).toBe(
+      recovered.address.toString({ urlSafe: true, bounceable: false })
+    );
     expect(keyVaultService.decrypt(wallet.privateKeyEncrypted)).toBe(
       recovered.privateKey
     );
--- src/KeyVaultService/KeyVaultService.ts
+++ src/KeyVaultService/KeyVaultService.ts
@@ -1,4 +1,7 @@
 import { Wallet } from 'ethers';
+import { mnemonicNew, mnemonicToWalletKey } from '@ton/crypto';
+import { WalletContractV5R1 } from '@ton/ton';
+import TonWeb from 'tonweb';
 import { AES256GCM } from '../utils/AES256GCM';
 import { KeyPair } from '../solana/KeyPair/KeyPair';
 
@@ -9,8 +12,9 @@ export class KeyVaultService extends AES256GCM {
         const wallet = KeyPair.generate();
         const hexPrivateKey = Buffer.from(wallet.secretKey).toString('hex');
         const encryptedPrivateKey = this.encrypt(hexPrivateKey);
-    
+
         return {
+          address: wallet.publicKey.toString(),
           publicKey: wallet.publicKey,
           privateKeyEncrypted: encryptedPrivateKey,
         };
@@ -37,6 +41,48 @@ export class KeyVaultService extends AES256GCM {
       recover: (encryptedPrivateKey: string) => {
         const decryptedHex = this.decrypt(encryptedPrivateKey);
         return new Wallet(decryptedHex);
+      },
+    };
+  }
+
+  get ton() {
+    return {
+      generate: async () => {
+        const mnemonic = await mnemonicNew(24);
+        const keyPair = await mnemonicToWalletKey(mnemonic);
+        const hexPrivateKey = Buffer.from(keyPair.secretKey).toString('hex');
+        const encryptedPrivateKey = this.encrypt(hexPrivateKey);
+
+        const wallet = WalletContractV5R1.create({
+          workchain: 0,
+          publicKey: keyPair.publicKey,
+        });
+
+        return {
+          address: wallet.address,
+          publicKey: keyPair.publicKey,
+          privateKeyEncrypted: encryptedPrivateKey,
+        };
+      },
+      recover: (encryptedPrivateKey: string) => {
+        const decryptedHex = this.decrypt(encryptedPrivateKey);
+
+        const keyPair = TonWeb.utils.nacl.sign.keyPair.fromSecretKey(
+          Buffer.from(decryptedHex, 'hex')
+        );
+
+        const publicKey = Buffer.from(keyPair.publicKey);
+
+        const wallet = WalletContractV5R1.create({
+          workchain: 0,
+          publicKey,
+        });
+
+        return {
+          address: wallet.address,
+          publicKey,
+          privateKey: decryptedHex,
+        };
       },
     };
   }
